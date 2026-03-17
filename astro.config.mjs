f3e700d2ec1239368f12b53f7860090b53275dce
bhaskar Jain <bjain43@gatech.edu>
2026-03-16 20:28:52 -0400

    updated site: 'https://bhaskar-jain.vercel.app'

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://bhaskar-jain.vercel.app',
 	integrations: [mdx(), sitemap()],
 	redirects: {
 		'/about': '/projects',
